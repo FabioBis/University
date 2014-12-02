e5c8433b597afd614153017ab86fe97f36718d9b
FabioBis <fabio.biselli.80@gmail.com>
2014-12-02 19:15:36 +0100

    Project tutorial step 5.

---
+++ UNIBO/SimulazioneDiSistemi/OMNet/tutorials/tictoc2/Txc5.cpp
@@ -0,0 +1,11 @@
+/*
+ * Txc5.cpp
+ *
+ *  Created on: 02/dic/2014
+ *      Author: Fabio
+ */
+
+#include <Txc5.h>
+
+// The module class need to be registered with OMNet++.
+Define_Module(Txc5);
---
+++ UNIBO/SimulazioneDiSistemi/OMNet/tutorials/tictoc2/Txc5.h
@@ -0,0 +1,16 @@
+/*
+ * Txc5.h
+ *
+ *  Created on: 02/dic/2014
+ *      Author: Fabio
+ */
+
+#ifndef TXC5_H_
+#define TXC5_H_
+
+#include <Txc4.h>
+
+class Txc5 : public Txc4 {
+};
+
+#endif /* TXC5_H_ */
