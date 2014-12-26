08ada12144cb4941a2a86e9ca4aea7ced9dd07bd
FabioBis <fabio.biselli.80@gmail.com>
2014-12-26 16:19:21 +0100

    Project tutorial step 12.

---
+++ UNIBO/SimulazioneDiSistemi/OMNet/tutorials/tictoc2/Txc12.cpp
@@ -0,0 +1,59 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include <Txc12.h>
+
+Define_Module(Txc12);
+
+void
+Txc12::initialize()
+{
+    if (getIndex() == 0)
+    {
+        char msgname[20];
+        sprintf(msgname, "tic-%d", getIndex());
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(0.0, msg);
+    }
+}
+
+void
+Txc12::handleMessage(cMessage *msg)
+{
+    if (getIndex() == 3)
+    {
+        // Message arrived.
+        EV << "Message " << msg <<" arrived.\n";
+        delete msg;
+    }
+    else
+    {
+        // We need to forward the message.
+        forwardMessage(msg);
+    }
+}
+
+void
+Txc12::forwardMessage(cMessage *msg)
+{
+    // In this example, we just pick a random gate to send it on.
+    int n = gateSize("gate");
+    EV << "Forwarding message...\n" <<"..Gate size: " << n <<";\n";
+    // We draw a random number between 0 and n-1.
+    int k = intuniform(0, n-1);
+    EV << ".." <<msg <<" on gate[" <<k <<"].\n";
+    // $o and $i suffix is used to identify the input/output part of a two way gate.
+    send(msg, "gate$o", k);
+}
