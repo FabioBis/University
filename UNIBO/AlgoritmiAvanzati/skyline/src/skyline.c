250a11e634c7e8db622e4eacc1775b0f7373ac0d
Fabio <fabio.biselli.80@gmail.com>
2015-08-10 19:09:28 +0200

    Some progress on Partitioning data.

--- UNIBO/AlgoritmiAvanzati/skyline/src/skyline.c
+++ UNIBO/AlgoritmiAvanzati/skyline/src/skyline.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 #include <string.h>
 #include <time.h>
 
@@ -89,7 +90,7 @@ void skyline(Point3D* In, int size_in, Point3D* Out, int& size_out)
 	return;
 }
 
-void getData(Point3D* Out, int& size_out)
+void getDataSize(int &size_out)
 {
 	FILE *f;
 
@@ -106,26 +107,53 @@ void getData(Point3D* Out, int& size_out)
 	getline (input_string, &bytes, f);
 	
 	size_out = atoi(input_string);
-	Out = (Point3D*)malloc(size_out * sizeof(Point3D));
+}
+
+void getData(Point3D* Out, int size)
+{
+	FILE *f;
+
+	f = fopen ("test.txt","r");  /* Input file. */
+	if (f == NULL)
+	{
+		printf ("Errore durante l'apertura del file\n");
+		return;
+	}
+	
+	size_t bytes = 50;
+	char* input_string;
+	input_string = (char *) malloc (nbytes + 1);
+	getline (input_string, &bytes, f);
+	
+	size_in = atoi(input_string);
 	
 	int i;
-	for (i = 0; i < size_out; i++)
+	for (i = 0; i < size; i++)
 	{
-		getline (input_string, &bytes, f);
-		char str[50], *s = str, *t = NULL;
-		strcpy(str, input_string);
-		
-		float coordinates[3];
-		int i;
-		for (i = 0; i < 3; i++)
+		if (i < size_in)
 		{
-			t = strtok(s, " ");
-			s = NULL;
-			coordinates[k] = atof(t);
+			getline (input_string, &bytes, f);
+			char str[50], *s = str, *t = NULL;
+			strcpy(str, input_string);
+			
+			float coordinates[3];
+			int i;
+			for (i = 0; i < 3; i++)
+			{
+				t = strtok(s, " ");
+				s = NULL;
+				coordinates[k] = atof(t);
+			}
+			Out[i].x = coordinates[0];
+			Out[i].y = coordinates[1];
+			Out[i].z = coordinates[2];
 		}
-		Out[i].x = coordinates[0];
-		Out[i].y = coordinates[1];
-		Out[i].z = coordinates[2];
+		else
+		{
+			Out[i].x = -FLT_MAX;
+			Out[i].y = -FLT_MAX;
+			Out[i].z = -FLT_MAX;
+		}
 	}
 	
 	free(input_string);
@@ -135,22 +163,78 @@ void getData(Point3D* Out, int& size_out)
 
 int main( int argc, char* argv[] )
 {
-	int my_rank, comm_sz;
+	int my_rank, comm_sz, input_size, partition_size;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	int active_proc = comm_sz; 
 	
-	/* TODO:
-	 *
-	 *  - input da file in array di Point3D;
-	 *    + modifica dell'array in base al numero N di processi
-	 *    + per avere N partizioni omogenee
-	 *  - Broadcast del nuovo array di Point3D a tutti gli N processi
-	 *  - Modificare la funzione skyline in modo che restituisca un array di
-	 *    interi da utilizzare con MPI_Reduce
-	 */
+	if (my_rank == 0)
+	{
+		getDataSize(input_size);
+	}
+	MPI_Bcast(input_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	
+	int actual_size, part_tag = 0;
+	if (input_size % comm_sz == 0)
+	{
+		partition_size = input_size / comm_sz;
+		actual_size = input_size;
+	}
+	else
+	{
+		partition_size = input_size / comm_sz + 1;
+		if (partition_size * (comm_sz + 1) == input_size)
+		{
+			part_tag = 1;
+			active_proc -=1;
+		}
+		else
+		{
+			part_tag = 2;
+		}
+		actual_size = partition_size * comm_sz;
+	}
+	
+	Point3D partition_set[partition_size];
+	
+	MPI_Datatype Point3Dstruct, Point3Dtype;
+	MPI_Datatype type[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
+	int blocklen[3] = {1, 1, 1};
+	MPI_Aint disp[3];
+	MPI_Aint base, lb, sizeofentry;
+	MPI_Get_address(partition_set, disp);
+	MPI_Get_address(partition_set[0].x, disp+1);
+	MPI_Get_address(partition_set[0].z, disp+2);
+	base = disp[0];
+	for (i=0; i < 3; i++) disp[i] = MPI_Aint_diff(disp[i], base);
+	
+	MPI_Type_create_struct(3, blocklen, disp, type, &Point3Dstruct);
+	
+	MPI_Get_address(partition_set+1, &sizeofentry);
+	sizeofentry = MPI_Aint_diff(sizeofentry, base);
+	MPI_Type_create_resized(Point3Dstruct, 0, sizeofentry, &Point3Dtype);
+	MPI_Type_commit(&Point3Dtype);
+	
+	Point3D actual_set[actual_size];
+	if ((my_rank == 0)
+	{
+		getData(actual_set, actual_size);
+	}	
+	MPI_Scatter(
+		actual_set, actual_size, Point3Dtype,
+		partition_set, partition_size, Point3Dtype,
+		0, MPI_COMM_WORLD);
+	
+	/* DBG */
+	printf("Process %d:\n", my_rank);	
+	for (int i = 0; i < partition_size; i++)
+	{
+		printf("P[%d]= <%f, %f, %f>\n", i, partition_set[i].x,
+			partition_set[i].y, partition_set[i].z);		
+	}
+	/* DBG */
 	
 	MPI_Finalize();
 
